aef3f6d02c6eaa95e782f30cf18f880f667c1150
coderjahidul <sobuz0349@gmail.com>
2025-06-16 12:20:34 +0600

    Remove non-numeric characters and Remove +88 or 880 from beginning

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
     function checkDSR(phone) {
-        console.log('Checking DSR for phone:', phone.length);
+        console.log('Checking DSR for phone:', phone);
         $.ajax({
             type: 'POST',
             url: cdc_ajax_object.ajax_url,
@@ -25,10 +25,16 @@ jQuery(function($) {
 
     // Monitor phone input field change (Woo default field ID)
     $(document).on('keyup', 'input[name="billing_phone"]', function() {
-        const phone = $(this).val();
-        console.log('Phone:', phone.length);
-        if (phone.length == 11) {
+        let phone = $(this).val().replace(/\D/g, ''); // Remove non-numeric characters
+        
+        // Remove +88 or 880 from beginning
+        if (phone.startsWith('88')) {
+            phone = phone.substring(2);
+        }
+    
+        // Check if phone number is now 11 digits
+        if (phone.length === 11) {
             checkDSR(phone);
         }
-    });
+    });  
 });
