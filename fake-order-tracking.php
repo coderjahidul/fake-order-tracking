d504b6ef37ac4748d384c10172138881f3f35f2b
coderjahidul <sobuz0349@gmail.com>
2025-07-02 17:21:07 +0600

    remove file_put_contents function

--- fake-order-tracking.php
+++ fake-order-tracking.php
@@ -84,34 +84,6 @@ function conditionally_enable_cod($available_gateways) {
 
     return $available_gateways;
 }
-// Function to append data to a log file
-function put_program_logs( $data ) {
-
-    // Ensure the directory for logs exists
-    $directory = __DIR__ . '/program_logs/';
-    if ( ! file_exists( $directory ) ) {
-        // Use wp_mkdir_p instead of mkdir
-        if ( ! wp_mkdir_p( $directory ) ) {
-            return "Failed to create directory.";
-        }
-    }
-
-    // Construct the log file path
-    $file_name = $directory . 'program_logs.log';
-
-    // Append the current datetime to the log entry
-    $current_datetime = gmdate( 'Y-m-d H:i:s' ); // Use gmdate instead of date
-    $data             = $data . ' - ' . $current_datetime;
-
-    // Write the log entry to the file
-    if ( file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-        return "Data appended to file successfully.";
-    } else {
-        return "Failed to append data to file.";
-    }
-}
-
-
 
 function cdc_run_plugin() {
     $plugin = new CDC_Loader();
